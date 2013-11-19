* bugfix: samples scaling * improved: schedule plotting * improved: workflow

analyze.py
@@ -96,9 +96,10 @@ if __name__ == '__main__':
 
     sched = sched.repeat(15, axis=1)
     ctrl_sched = np.zeros((unctrl.shape[0], unctrl.shape[-1]))
-    ctrl_sched[:,:pre.shape[-1]] = pre[:,0]
+    ctrl_sched = np.ma.array(ctrl_sched)
+    ctrl_sched[:,:pre.shape[-1]] = np.ma.masked
     ctrl_sched[:,pre.shape[-1]:pre.shape[-1] + sched.shape[-1]] = sched
-    ctrl_sched[:,pre.shape[-1] + sched.shape[-1]:] = post[:,0]
+    ctrl_sched[:,pre.shape[-1] + sched.shape[-1]:] = np.ma.masked
 
     # plot_each_device(sc, unctrl, ctrl, sched)
     plot_aggregated(sc, unctrl, ctrl, ctrl_sched)

simulator.py
@@ -44,7 +44,7 @@ def create_sample(device, sample_size, t_start, t_end, progress, density=0.1):
     scale = 0.000001 * np.max(np.abs(sample))
     noise = np.random.normal(scale=scale, size=(sample_size, d))
 
-    sample = (sample / 1000) + noise
+    sample = sample + noise
 
     if device.typename == 'heatpump':
         # This is a consumer, so negate the sample